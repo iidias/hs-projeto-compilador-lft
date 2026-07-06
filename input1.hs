c068a15163c659d36ea728101ca23718f3c66d7f
Yasmim <88737080+yasmim-passos@users.noreply.github.com>
2026-07-06 09:47:18 -0300

    Add files via upload

---
+++ input1.hs
@@ -0,0 +1,25 @@
+data Forma = Circulo Int
+           | Retangulo Int Int
+
+area :: Forma -> Int
+area f = case f of
+    Circulo r     -> r * r
+    Retangulo l a -> l * a
+
+classificar :: Int -> String
+classificar n
+    | n < 0     = "negativo"
+    | n == 0    = "zero"
+    | otherwise = "positivo"
+
+fatorial :: Int -> Int
+fatorial 0 = 1
+fatorial n = n * fatorial n
+
+dobrar :: Int -> Int
+dobrar = \x -> x + x
+
+main :: IO ()
+main = do
+    let r = area (Circulo 5)
+    return r
